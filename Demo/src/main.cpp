d03c556f4757e2e96ca0ed9c6ed60b525d4e17cc
mjBarahona <b9021848@my.shu.ac.uk>
2020-04-22 21:02:45 +0000

    FinalCommit 2

--- Demo/src/main.cpp
+++ Demo/src/main.cpp
@@ -10,7 +10,7 @@ int main() {
 	C3D::ObjData data;
 	uint8_t i = 10;
 
-	C3D::Compressor::CompressModel("Shirakawago_House_1.obj", "Shirakawago_House_1", i);
+	C3D::Compressor::CompressModel("../../Corpus3DMeshes/Shirakawago_House_1.obj", "Shirakawago_House_1", i);
 	C3D::Compressor::DecompressModel("Shirakawago_House_1.C3D", data);
 
 	return 0;
